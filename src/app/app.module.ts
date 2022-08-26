ffe6c6d0ec25a2b377e891e75a2075e9f0095c5e
Prateek Soni <prateek_soni@outlook.com>
2022-08-26 19:04:48 +0530

    refactor appointment
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule
